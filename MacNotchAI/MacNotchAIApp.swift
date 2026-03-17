2e1175f248a40edfa917f1868146bc993b502278
mwallbrecher <moritz.wallbrecher2@gmail.com>
2026-03-17 14:19:03 +0000

    Initial Commit

---
+++ MacNotchAI/MacNotchAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacNotchAIApp.swift
+//  MacNotchAI
+//
+//  Created by Moritz Wallbrecher on 17.03.26.
+//
+
+import SwiftUI
+
+@main
+struct MacNotchAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
